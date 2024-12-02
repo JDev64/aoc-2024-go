ea6d6293a0dc1886a9c47ba45dfd94362f496178
jadenpape <jaden.pape@check24.de>
2024-12-02 21:32:25 +0100

    update start script

--- run.sh
+++ run.sh
@@ -11,26 +11,27 @@ function template() {
 package main
 
 import (
-	"github.com/jpillora/puzzler/harness/aoc"
+	"os"
 )
 
 func main() {
-	aoc.Harness(run)
+	fileContent, err := os.ReadFile("input-user.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	inputString := string(fileContent)
+
+	part1(inputString)
+	part2(inputString)
 }
 
-// on code change, run will be executed 4 times:
-// 1. with: false (part1), and example input
-// 2. with: true (part2), and example input
-// 3. with: false (part1), and user input
-// 4. with: true (part2), and user input
-// the return value of each run is printed to stdout
-func run(part2 bool, input string) any {
-	// when you're ready to do part 2, remove this "not implemented" block
-	if part2 {
-		return "not implemented"
-	}
-	// solve part 1 here
-	return 42
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+	return 0
 }
 EOF
 }
@@ -55,4 +56,7 @@ if [ ! -f "$DIR/code.go" ]; then
 	echogrey "Created file code.go"
 fi
 # go run
-cd "$DIR" && go run code.go
+cd "$DIR"
+touch input-example.txt
+touch input-user.txt
+
